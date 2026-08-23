c93718c2ce5053bb2203db4bc030e4ca2b0af756
Shivamkr-cod <namkeenshivam156@gmail.com>
2026-08-23 16:36:22 +0530

    Refactor recurring transactions into trigger and process Inngest functions to match tutorial exactly

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,12 @@
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
-import { checkBudgetAlert, triggerRecurringTransactions } from "@/lib/inngest/functions";
+import { checkBudgetAlert, triggerRecurringTransactions, processRecurringTransaction } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     checkBudgetAlert,
     triggerRecurringTransactions,
+    processRecurringTransaction,
   ],
 });
