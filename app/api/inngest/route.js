af3d94096e9827fb5f1fe63d76a890ff978feeb4
Shivamkr-cod <namkeenshivam156@gmail.com>
2026-08-25 01:25:26 +0530

    Fix checkBudgetAlerts import name to match updated functions.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,11 @@
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
-import { checkBudgetAlert, triggerRecurringTransactions, processRecurringTransaction, generateMonthlyReports } from "@/lib/inngest/functions";
+import { checkBudgetAlerts, triggerRecurringTransactions, processRecurringTransaction, generateMonthlyReports } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    checkBudgetAlert,
+    checkBudgetAlerts,
     triggerRecurringTransactions,
     processRecurringTransaction,
     generateMonthlyReports,
